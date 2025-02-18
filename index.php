9535829fdc2ed149d8fc51f459cc62f72f51cb1e
JEGANCHRIS <jmchristo.2000@gmail.com>
2025-02-18 15:20:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+<title>Bike Portal</title>
+<style>
+
+p
+{
+	color:#ffffff;
+	text-align: center;
+	text-transform: uppercase;
+	 font-size:15px;
+}
+
+ul {
+	padding:30px;
+	
+  list-style-type: none;
+  overflow: hidden;
+  background:black;
+   background-repeat: no-repeat;
+   background-size: 1420px  200px;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+   border-radius:0px;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: #ffffff;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  border-radius:8px;
+}
+
+li a:hover {
+  background-color: grey;
+}
+
+.active {
+  background-color: brown;
+}
+
+#footer {
+   padding: 30px;
+  background: black;
+  background-repeat: no-repeat;
+  background-size: 1420px  100px;
+  border-radius:10px;
+  text-align:center;
+  text-decoration:blink;
+   font-family: Arial;
+   font-size:15px;
+}
+#bg1 {
+
+  padding:150px 250px 400px 20px;
+  background:url("images/re.jpg");
+  background-repeat: no-repeat;
+  background-size: 1420px  600px;
+  border-radius:5px;
+   border-radius:10px;
+    text-align:top-left;
+   font-size:60px;
+   color:#ffffff;
+   color:white;
+  
+}
+
+</style>
+</head>
+<ul>
+	 <li><a href="#">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a></li>
+  <li><a class="active" href="index.php"><strong>Home Page</a></li>
+  <li><a href="#">&nbsp;</a></li>
+  <li><a href="admin.php"><strong>Admin login</a></li>
+   <li><a href="#">&nbsp;</a></li>
+  <li><a href="user.php"><strong>User login</a></li>
+   <li><a href="#">&nbsp;</a></li>
+  <li><a href="register.php"><strong>New User</a></li>
+   <li><a href="#">&nbsp;</a></li>
+   <li><a href="about.php"><strong>About Us</a></li>
+</ul>
+
+<br />
+<br />
+<div id="bg1">Online Bike Rental Portal</div>
+
+
+
+<div> &nbsp;</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id="footer"> <p>copyrights & designedby@Bike Portal</p>
+</div>
---
+++ viewu.php
@@ -0,0 +1,168 @@
+
+<?php
+ 	include("dbconnect.php");
+	extract($_POST);
+	session_start();
+	?>
+<html>
+<title>Bike Portal</title>
+<style>
+
+p
+{
+	color:#ffffff;
+	text-align: center;
+	text-transform: uppercase;
+	 font-size:15px;
+}
+
+ul {
+	padding:30px;
+	
+  list-style-type: none;
+  overflow: hidden;
+  background:black;
+   background-repeat: no-repeat;
+   background-size: 1420px  200px;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+   border-radius:0px;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: #ffffff;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  border-radius:8px;
+}
+
+li a:hover {
+  background-color: brown;
+}
+
+.active {
+  background-color: brown;
+}
+
+#footer {
+   padding: 30px;
+  background:black;
+  background-repeat: no-repeat;
+  background-size: 1420px  100px;
+  border-radius:10px;
+  text-align:center;
+  text-decoration:blink;
+   font-family: Arial;
+   font-size:15px;
+}
+#bg1 {
+
+  padding:150px 250px 400px 20px;
+  background:url("images/re.jpg");
+  background-repeat: no-repeat;
+  background-size: 1420px  600px;
+  border-radius:5px;
+   border-radius:10px;
+    text-align:left;
+   font-size:60px;
+   color:#ffffff;
+   color:white;
+}
+
+</style>
+</head>
+<ul>
+	 <li><a href="#">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a></li>
+  <li><a class="active" href="adminhome.php"><strong>Admin Home</a></li>
+  <li><a href="#">&nbsp;</a></li>
+  <li><a href="booking.php"><strong>Booking Details</a></li>
+   <li><a href="#">&nbsp;</a></li>
+  <li><a href="viewu.php"><strong>User Details</a></li>
+   <li><a href="#">&nbsp;</a></li>
+   <li><a href="index.php"><strong>LogOut</a></li>
+</ul>
+
+<br />
+<br />
+<div id="bg1">Online Bike Rental Portal</div>
+
+
+<table width="98%" align="center">
+
+<tr>
+		<td colspan="10" align="center"><strong>User Details</strong></td>
+  </tr>
+
+	<tr>
+		<td colspan="10">&nbsp;</td>
+  </tr>
+	
+		    <tr>
+          <td width="1%">&nbsp;</td>
+          <td width="7%"><div align="center" class="style6"><strong>Id</strong> </div></td>
+		   <td width="11%"><div align="center" class="style6"><strong>User Name</strong> </div></td>
+		   <td width="11%"><div align="center" class="style6"><strong>Gender</strong> </div></td>
+		    <td width="11%"><div align="center" class="style6"><strong>Age</strong> </div></td>
+			  <td width="9%"><div align="center" class="style6"><strong>Email</strong> </div></td>
+			   <td width="10%"><div align="center" class="style6"><strong>Contact No</strong> </div></td>
+			     <td width="10%"><div align="center" class="style6"><strong>Address</strong> </div></td>
+				  </tr>
+		<tr>
+		<td colspan="10">&nbsp;</td>
+		</tr>
+		<?php
+		$qry=mysql_query("select * from register");
+		$i=1;
+		while($row=mysql_fetch_array($qry))
+		{
+		?>
+
+        <tr>
+		 <td width="1%">&nbsp;</td>
+		    <td><div align="center"><?php echo $row['id'];?></div></td>
+		   <td><div align="center"><?php echo $row['name'];?></div></td>
+			  <td><div align="center"><?php echo $row['gender'];?></div></td>
+			   <td><div align="center"><?php echo $row['age'];?></div></td>
+				 <td><div align="center"><?php echo $row['email'];?></div></td>
+				  <td><div align="center"><?php echo $row['phone'];?></div></td>
+				   <td><div align="center"><?php echo $row['address'];?></div></td>
+
+		 
+        </tr>
+		
+		
+		 <tr>
+		  	 	<td>&nbsp;</td>
+		   		<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			 	<td>&nbsp;</td>
+			 	<td>&nbsp;</td>
+			 	<td>&nbsp;</td>
+		
+		
+		</tr>
+		
+        <?php
+		$i++;
+		}
+		?>
+				<tr>
+
+		<td colspan="10" align="center">&nbsp;</td>
+		</tr>
+		
+</table>
+
+
+
+
+
+<div id="footer"> <p>copyrights & designedby@Bike Portal</p>
+</div>
